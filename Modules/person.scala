d0c13be5d4fc8fc8df44dcac936edc3bafdbbd25
genedan <genedan@gmail.com>
2018-01-18 00:39:58 -0600

    moved class files to Modules folder, declared triangle class

---
+++ Modules/person.scala
@@ -0,0 +1,9 @@
+//People will be the fundamental type of agent for this engine, and person will be a class
+
+//On the company side, subclasses of people will include adjusters, underwriters, actuaries, and other employees
+
+//On the consumer side, we'll have policyholders and claimants
+
+//People should be able to be grouped into larger classes, such as households, departments, cities, etc.
+
+def person(){}
---
+++ Modules/triangle.scala
@@ -0,0 +1,4 @@
+/*Declare a class for triangle. Triangles contain premium, loss, and expense information - a data structure used to develop known values to ultimate */
+
+def triangle(){}
+

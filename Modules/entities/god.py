278a100a4eb994251b5f652b9c9f622385ee5889
genedan <genedan@gmail.com>
2020-06-15 22:47:12 -0500

    move god class

---
+++ Modules/entities/god.py
@@ -0,0 +1,101 @@
+import os
+import pandas as pd
+import parameters as pm
+
+from scipy.stats import gamma
+from scipy.stats import pareto
+from numpy.random import poisson
+
+# The supreme entity, overseer of all space, time, matter and energy
+class God:
+
+    def __init__(self, session, engine):
+        self.session = session
+        self.connection = engine.connect()
+
+    def make_person(self):
+        self.make_population(1)
+
+    def make_population(self, n_people):
+        age_class = pm.draw_ac(n_people)
+        profession = pm.draw_prof(n_people)
+        health_status = pm.draw_hs(n_people)
+        education_level = pm.draw_el(n_people)
+        income = pareto.rvs(
+            b=1,
+            scale=pm.person_params['income'],
+            size=n_people,
+        )
+        cobb_c = [pm.person_params['cobb_c']] * n_people
+        cobb_d = [pm.person_params['cobb_d']] * n_people
+
+        population = pd.DataFrame(list(
+            zip(
+                age_class,
+                profession,
+                health_status,
+                education_level,
+                income,
+                cobb_c,
+                cobb_d
+            )
+        ), columns=[
+            'age_class',
+            'profession',
+            'health_status',
+            'education_level',
+            'income',
+            'cobb_c',
+            'cobb_d'
+        ])
+
+        population.to_sql(
+            'person',
+            self.connection,
+            index=False,
+            if_exists='append'
+        )
+
+    def smite(
+        self,
+        person,
+        policy,
+        ev_date
+    ):
+
+        population = pd.read_sql(self.session.query(person, policy.policy_id).outerjoin(policy).filter(
+                policy.effective_date <= ev_date
+            ).filter(
+                policy.expiration_date >= ev_date
+            ).statement, self.connection)
+
+        population['lambda'] = pm.get_poisson_lambda(population)
+        population['frequency'] = poisson(population['lambda'])
+
+        population = population[population['frequency'] != 0]
+        population['event_date'] = ev_date
+
+        population = population.loc[population.index.repeat(population.frequency)].copy()
+
+        population['gamma_scale'] = pm.get_gamma_scale(population)
+        population['severity'] = gamma.rvs(
+            a=2,
+            scale=population['gamma_scale']
+        )
+
+        population = population[['event_date', 'person_id', 'policy_id', 'severity']]
+        population.to_sql(
+            'event',
+            self.connection,
+            index=False,
+            if_exists='append'
+        )
+        return population
+
+    def annihilate(self, db):
+        os.remove(db)
+
+
+
+
+

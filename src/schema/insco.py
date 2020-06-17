a34c36a0031312590c1f67b6920a1614c448c21c
genedan <genedan@gmail.com>
2020-06-17 00:55:28 -0500

    move insurance company schema to new module

---
+++ src/schema/insco.py
@@ -0,0 +1,47 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Date, Float
+from sqlalchemy import ForeignKey
+
+
+Base = declarative_base()
+
+
+class Policy(Base):
+    __tablename__ = 'policy'
+
+    policy_id = Column(
+        Integer,
+        primary_key=True
+    )
+    company_id = Column(
+        Integer
+    )
+    person_id = Column(Integer)
+    effective_date = Column(Date)
+    expiration_date = Column(Date)
+    premium = Column(Float)
+
+    def __repr__(self):
+        return "<Policy(person_id ='%s'," \
+               "effective_date ='%s', " \
+               "expiration_date='%s', " \
+               "premium='%s')>" % (
+                self.person_id,
+                self.effective_date,
+                self.expiration_date,
+                self.premium
+                )
+
+
+class Claim(Base):
+    __tablename__ = 'claim'
+
+    claim_id = Column(
+        Integer,
+        primary_key=True
+    )
+    policy_id = Column(
+        Integer,
+        ForeignKey('policy.policy_id')
+    )
+    loss = Column(Float)

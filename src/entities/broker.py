27419aaaa2e6bea70844ec2ab542b51849fc9bf4
genedan <genedan@gmail.com>
2020-06-20 02:05:55 -0500

    add method to report claims

--- src/entities/broker.py
+++ src/entities/broker.py
@@ -1,15 +1,16 @@
 import pandas as pd
-import numpy as np
 import datetime
-
 
 from random import choices
 from parameters import INITIAL_PREMIUM
 from schema.universe import Company
 from utilities.connections import connect_universe
 from utilities.connections import connect_company
+from utilities.queries import get_company_names
+from utilities.queries import query_company
+from utilities.queries import query_all_policies
 from schema.insco import Policy
-from schema.universe import PersonTable
+from schema.universe import Event, PersonTable
 
 
 class Broker:
@@ -19,7 +20,7 @@ class Broker:
             curr_date
     ):
         # get expiring policies for each company
-        companies = self.identify_companies()
+        companies = get_company_names()
         market = pd.DataFrame()
         for company in companies:
 
@@ -75,7 +76,7 @@ class Broker:
         else:
             market_status = 'renewal'
 
-        companies = self.query_company()
+        companies = query_company()
 
         if market_status == 'initial_pricing':
             free_business['company_id'] = choices(companies.company_id, k=len(free_business))
@@ -84,7 +85,7 @@ class Broker:
             free_business['expiration_date'] = curr_date.replace(curr_date.year + 1)
 
             for company in companies.company_id:
-                company_name = companies[companies['company_id'] == company]['name']
+                company_name = companies[companies['company_id'] == company]['company_name']
                 company_name = company_name.squeeze()
                 new_business = free_business[free_business['company_id'] == company]
                 new_business = new_business[[
@@ -131,20 +132,29 @@ class Broker:
             # return free_business
             return None
 
-    def identify_companies(self):
+    def report_claims(self, report_date):
         session, connection = connect_universe()
-        companies_query = session.query(Company.name).statement
-        companies = pd.read_sql(companies_query, connection)
-        return list(companies['name'])
+        event_query = session.query(Event).filter(Event.report_date == report_date).statement
+        events = pd.read_sql(event_query, connection)
+        connection.close()
 
-    def get_company_ids(self):
-        session, connection = connect_universe()
-        companies_query = session.query(Company.company_id).statement
-        companies = pd.read_sql(companies_query, connection)
-        return list(companies['company_id'])
+        policies = query_all_policies()
+        claims = events.merge(policies, on='person_id', how='left')
+        claims = claims.query('event_date >= effective_date and event_date <= expiration_date')
 
-    def query_company(self):
-        session, connection = connect_universe()
-        companies_query = session.query(Company).statement
-        companies = pd.read_sql(companies_query, connection)
-        return companies
+        claims = claims.drop(['effective_date', 'expiration_date', 'premium', 'company_id'], axis=1)
+
+        companies = get_company_names()
+        for company in companies:
+            reported_claims = claims[claims['company_name'] == company]
+            reported_claims = reported_claims.rename(columns={'event_date': 'occurrence_date', 'ground_up_loss': 'incurred_loss'})
+            reported_claims = reported_claims.drop(['company_name'], axis=1)
+            session, connection = connect_company(company)
+            reported_claims.to_sql(
+                'claim',
+                connection,
+                index=False,
+                if_exists='append'
+            )
+            connection.close()
+

1aa722a070504e4c2e4d82d6d77d15f5f0001277
genedan <genedan@gmail.com>
2020-06-21 00:18:03 -0500

    add in force, uninsured, and customer queries

--- src/utilities/queries.py
+++ src/utilities/queries.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 from schema.universe import Company, PersonTable
-from schema.insco import Policy
-from utilities.connections import(
+from schema.insco import Customer, Policy
+from utilities.connections import (
     connect_universe,
     connect_company)
 
@@ -47,6 +47,32 @@ def query_all_policies():
     return policies
 
 
+def query_in_force_policies(curr_date):
+    companies = get_company_names()
+    in_force = pd.DataFrame()
+
+    for company in companies:
+        session, connection = connect_company(company)
+
+        exp_query = session.query(
+            Policy
+        ).filter(
+            Policy.expiration_date == curr_date
+        ).statement
+
+        company_in_force = pd.read_sql(
+            exp_query,
+            connection)
+
+        in_force = in_force.append(
+            company_in_force,
+            ignore_index=True
+        )
+
+        connection.close()
+    return in_force
+
+
 def get_company_names():
     session, connection = connect_universe()
     companies_query = session.query(Company.company_name).statement
@@ -65,3 +91,16 @@ def get_company_ids():
     connection.close()
     return list(companies['company_id'])
 
+
+def get_uninsured_ids(curr_date):
+    population = query_population()
+    in_force = query_in_force_policies(curr_date)
+    uninsureds = population[~population['person_id'].isin(in_force['person_id'])]['person_id']
+    return uninsureds
+
+
+def get_customer_ids(company):
+    session, connection = connect_company(company)
+    id_query = session.query(Customer.person_id).statement
+    ids = pd.read_sql(id_query, connection)
+    return ids

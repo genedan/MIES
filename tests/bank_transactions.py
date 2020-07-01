b80edb17b2247fec976f93b5070da645a2a5ed21
genedan <genedan@gmail.com>
2020-07-01 00:20:13 -0500

    update transactions unit test

--- tests/bank_transactions.py
+++ tests/bank_transactions.py
@@ -1,4 +1,6 @@
 import datetime as dt
+from entities.broker import Broker
+from entities.insurer import Insurer
 from utilities.queries import query_population
 from entities.bank import Bank
 from entities.god import God
@@ -20,6 +22,22 @@ customer_ids = query_customers_by_person_id(ids, 'blargo')
 blargo.assign_accounts(customer_ids=customer_ids, account_type='cash')
 ahura.grant_wealth(person_ids=ids, bank=blargo, transaction_date=pricing_date)
 ahura.send_paychecks(person_ids=ids, bank=blargo, transaction_date=pricing_date)
+
+rayon = Broker()
+
+company_1 = Insurer(4000000, blargo, pricing_date, 'company_1')
+
+company_1_formula = 'incurred_loss ~ ' \
+                    'age_class + ' \
+                    'profession + ' \
+                    'health_status + ' \
+                    'education_level'
+
+rayon.place_business(
+        pricing_date,
+        blargo,
+        company_1
+    )
 
 test = query_population_wealth()
 

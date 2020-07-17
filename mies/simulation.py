e3755c42392e99739314539342440c217b896c2f
genedan <genedan@gmail.com>
2020-07-17 00:00:00 -0500

    endowment income effect, endowment condition toggle

--- mies/simulation.py
+++ mies/simulation.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import datetime as dt
 
-from entities.god import God
-from entities.broker import Broker
-from entities.insurer import Insurer
+from mies.entities.god import God
+from mies.entities.bank import Bank
+from mies.entities.broker import Broker
+from mies.entities.insurer import Insurer
+from mies.utilities.queries import query_population, query_customers_by_person_id
 
 
 pd.set_option('display.max_columns', None)
@@ -15,16 +17,12 @@ ahura.make_population(1000)
 
 pricing_date = dt.date(1, 12, 31)
 
+blargo = Bank(4000000, 'blargo')
 rayon = Broker()
 
-company_1 = Insurer(4000000, 'company_1')
+company_1 = Insurer(4000000, blargo, pricing_date, 'company_1')
 
-company_2 = Insurer(4000000, 'company_2')
-
-#company_3 = Insurer(gsession, engine, 4000000, Company, 'company_3')
-#rands = Insurer(gsession, engine, 4000000, Company, 'rands')
-#sevrsp = Insurer(gsession, engine, 4000000, Company, 'sevresp')
-#company_4 = Insurer(gsession, engine, 4000000, Company, 'company_4')
+company_2 = Insurer(4000000, blargo, pricing_date, 'company_2')
 
 company_1_formula = 'incurred_loss ~ ' \
                     'age_class + ' \
@@ -34,10 +32,16 @@ company_1_formula = 'incurred_loss ~ ' \
 
 company_2_formula = 'incurred_loss ~' \
                     ' age_class'
-#company_3_formula = 'severity ~ age_class + profession'
-#rands_formula = 'severity ~ rands'
-#sevrsp_formula = 'severity ~ sevresp'
-#company_4_formula = 'severity ~ age_class + profession + health_status'
+
+
+population = query_population()
+ids = population['person_id']
+
+blargo.get_customers(ids=ids, customer_type='person')
+customer_ids = query_customers_by_person_id(ids, 'blargo')
+blargo.assign_accounts(customer_ids=customer_ids, account_type='cash')
+ahura.grant_wealth(person_ids=ids, bank=blargo, transaction_date=pricing_date)
+
 
 
 policy_count = pd.DataFrame(columns=['year', 'company_1', 'company_2', 'company_1_prem', 'company_2_prem'])
@@ -45,6 +49,7 @@ for i in range(50):
 
     rayon.place_business(
         pricing_date,
+        blargo,
         company_1,
         company_2
     )
@@ -55,11 +60,18 @@ for i in range(50):
 
     rayon.report_claims(event_date)
 
+    company_1.pay_claims(event_date + dt.timedelta(days=1))
+
+    company_2.pay_claims(event_date + dt.timedelta(days=1))
+
     company_1.price_book(company_1_formula)
 
     company_2.price_book(company_2_formula)
 
     pricing_date = pricing_date.replace(pricing_date.year + 1)
+
+
+    ahura.send_paychecks(person_ids=ids, bank=blargo, transaction_date=pricing_date)
 
     policy_count = policy_count.append({
         'year': pricing_date.year,
@@ -71,17 +83,10 @@ for i in range(50):
 
 policy_count = policy_count.groupby(['year'])[['company_1',
                                                'company_2',
-                                               # 'company_3',
-                                               #'rands',
-                                               # 'company_4',
                                                'company_1_prem',
                                                'company_2_prem'
-                                               # 'company_3_prem',
-                                               #'rands_prem',
-                                               # 'company_4_prem'
                                                ]].mean().reset_index()
 
-policy_count
 
 import plotly.graph_objects as go
 fig = go.Figure()
@@ -94,23 +99,20 @@ fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_2'],
                          mode='lines',
                          name='Company 2'))
 
-# fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_3'],
-#                          mode='lines',
-#                          name='Company 3'))
-
-# fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['rands'],
-#                          mode='lines',
-#                          name='rands'))
-
-# fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_4'],
-#                          mode='lines',
-#                          name='Company 4'))
-
 fig.update_layout(title='Insurer Market Share',
                   title_x=0.5,
                   xaxis_title='Underwriting Period',
                   yaxis_title='Policy Count',
                   yaxis_range=[0, 1000])
+
+fig['layout'].update({
+            'title_x': 0.45,
+            'width': 550,
+            'height': 400,
+            'margin': {
+                'l':10
+            }
+})
 
 fig.write_html('first_figure3.html', auto_open=True)
 
@@ -124,16 +126,6 @@ fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_1'],
 fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_2'],
                          mode='lines',
                          name='Company 2'))
-
-# fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_3'],
-#                          mode='lines',
-#                          name='Company 3'))
-#
-#
-#
-# fig.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_4'],
-#                          mode='lines',
-#                          name='Company 4'))
 
 fig.update_layout(title='Insurer Market Share',
                   title_x=0.5,
@@ -153,14 +145,6 @@ fig2.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_2_prem
                          mode='lines',
                          name='Company 2'))
 
-# fig2.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_3_prem'],
-#                          mode='lines',
-#                          name='Company 3'))
-#
-#
-# fig2.add_trace(go.Scatter(x=policy_count['year'], y=policy_count['company_4_prem'],
-#                          mode='lines',
-#                          name='Company 4'))
 
 fig2.update_layout(title='Average Premium per Policy',
                   title_x=0.5,

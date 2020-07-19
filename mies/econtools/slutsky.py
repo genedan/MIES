101ee8df4355c18e1b35dcb5e9c4c56a172d0781
genedan <genedan@gmail.com>
2020-07-18 22:52:37 -0500

    add plotting for endowment income effect, fix bugs

--- mies/econtools/slutsky.py
+++ mies/econtools/slutsky.py
@@ -21,6 +21,7 @@ class Slutsky:
         self.new_budget = new_budget
         self.new_budget.name = 'New Budget'
         self.utility = utility_function
+        self.endowment_mode = self.__toggle_endowment()
 
         self.old_bundle = self.utility.optimal_bundle(
             self.old_budget.good_x.price,
@@ -32,14 +33,25 @@ class Slutsky:
         self.pivoted_budget = self.calculate_pivoted_budget()
         self.substitution_bundle = self.calculate_substitution_bundle()
         self.substitution_effect = self.calculate_substitution_effect()
-        self.new_bundle = self.calculate_new_bundle()
+        self.ordinary_income_budget = self.calculate_ordinary_income_budget()
+        self.income_bundle = self.calculate_ordinary_income_bundle()
         self.income_effect = self.calculate_income_effect()
+        self.endowment_income_bundle = self.calculate_endowment_income_bundle()
         self.endowment_income_effect = self.calculate_endowment_income_effect()
         self.total_effect = self.substitution_effect + self.income_effect
         self.substitution_rate = self.calculate_substitution_rate()
         self.income_rate = self.calculate_income_rate()
         self.slutsky_rate = self.substitution_rate - self.income_rate
         self.plot = self.get_slutsky_plot()
+
+    def __toggle_endowment(self):
+        """
+        Detect whether endowment is present
+        """
+        if (self.old_budget.endowment is not None) and (self.new_budget.endowment is not None):
+            return True
+        else:
+            return False
 
     def calculate_pivoted_budget(self):
         """
@@ -70,30 +82,55 @@ class Slutsky:
         substitution_effect = self.substitution_bundle[0] - self.old_bundle[0]
         return substitution_effect
 
-    def calculate_new_bundle(self):
+    def calculate_ordinary_income_budget(self):
+        ordinary_income_budget = Budget(
+            good_x=self.new_budget.good_x,
+            good_y=self.new_budget.good_y,
+            income=self.old_budget.income,
+            name='Ordinary Income Budget')
+
+        return ordinary_income_budget
+
+    def calculate_ordinary_income_bundle(self):
         """
         Shift the budget line outward
         """
-        new_bundle = self.utility.optimal_bundle(
+        # if endowment present, fix income at old income
+        if self.endowment_mode:
+            income = self.old_budget.income
+        else:
+            income = self.new_budget.income
+
+        income_bundle = self.utility.optimal_bundle(
             self.new_budget.good_x.price,
             self.new_budget.good_y.price,
-            self.new_budget.income
+            income
         )
-        return new_bundle
+        return income_bundle
 
     def calculate_income_effect(self):
-        income_effect = self.new_bundle[0] - self.substitution_bundle[0]
+        income_effect = self.income_bundle[0] - self.substitution_bundle[0]
         return income_effect
 
     def calculate_endowment_income_bundle(self):
         # only works if both old and new budget were defined with an endowment
-        if (self.old_budget.endowment is None) and (self.new_budget.endowment is None):
-            return 0
+        if self.endowment_mode:
+            endowment_income_bundle = self.utility.optimal_bundle(
+                self.new_budget.good_x.price,
+                self.new_budget.good_y.price,
+                self.new_budget.income
+            )
+            return endowment_income_bundle
+        else:
+            return None
 
     def calculate_endowment_income_effect(self):
 
         # only works if both old and new budget were defined with an endowment
-        if (self.old_budget.endowment is None) and (self.new_budget.endowment is None):
+        if self.endowment_mode:
+            endowment_income_effect = self.endowment_income_bundle[0] - self.income_bundle[0]
+            return endowment_income_effect
+        else:
             return 0
 
     def calculate_substitution_rate(self):
@@ -112,6 +149,7 @@ class Slutsky:
     def get_slutsky_plot(self):
         max_x_int = max(
             self.old_budget.income / self.old_budget.good_x.price,
+            self.old_budget.income / self.new_budget.good_x.price,
             self.pivoted_budget.income / self.pivoted_budget.good_x.price,
             self.new_budget.income / self.new_budget.good_x.price
         ) * 1.2
@@ -124,10 +162,12 @@ class Slutsky:
 
         # interval boundaries
         effect_boundaries = [
-            self.new_bundle[0],
+            self.income_bundle[0],
             self.substitution_bundle[0],
-            self.old_bundle[0]
+            self.old_bundle[0],
+            self.endowment_income_bundle[0] if self.endowment_mode else None
         ]
+        effect_boundaries = list(filter(None.__ne__, effect_boundaries))
         effect_boundaries.sort()
 
         fig = go.Figure()
@@ -136,6 +176,8 @@ class Slutsky:
         fig.add_trace(self.old_budget.get_line())
         fig.add_trace(self.pivoted_budget.get_line())
         fig.add_trace(self.new_budget.get_line())
+        if self.endowment_mode:
+            fig.add_trace(self.ordinary_income_budget.get_line())
 
         # utility curves
         fig.add_trace(
@@ -154,11 +196,19 @@ class Slutsky:
         )
         fig.add_trace(
             self.utility.trace(
-                k=self.new_bundle[2],
+                k=self.income_bundle[2],
                 m=max_x_int,
-                name='New Utility'
+                name='Ordinary Income Utility' if self.endowment_mode else 'New Utility'
             )
         )
+        if self.endowment_mode:
+            fig.add_trace(
+                self.utility.trace(
+                    k=self.endowment_income_bundle[2],
+                    m=max_x_int,
+                    name='New Utility'
+                )
+            )
         # consumption bundles
 
         fig.add_trace(
@@ -193,10 +243,10 @@ class Slutsky:
 
         fig.add_trace(
             go.Scatter(
-                x=[self.new_bundle[0]],
-                y=[self.new_bundle[1]],
+                x=[self.income_bundle[0]],
+                y=[self.income_bundle[1]],
                 mode='markers+text',
-                text=['New Bundle'],
+                text=['Ordinary Income Bundle' if self.endowment_mode else 'New Bundle'],
                 textposition='top center',
                 marker=dict(
                     size=[15],
@@ -205,6 +255,22 @@ class Slutsky:
                 showlegend=False
             )
         )
+        if self.endowment_mode:
+            fig.add_trace(
+                go.Scatter(
+                    x=[self.endowment_income_bundle[0]],
+                    y=[self.endowment_income_bundle[1]],
+                    mode='markers+text',
+                    text=['New Bundle'],
+                    textposition='top center',
+                    marker=dict(
+                        size=[15],
+                        color=[3]
+                    ),
+                    showlegend=False
+                )
+            )
+
         # Substitution and income effect interval lines
         fig.add_shape(
             type='line',
@@ -221,9 +287,9 @@ class Slutsky:
 
         fig.add_shape(
             type='line',
-            x0=self.new_bundle[0],
-            y0=self.new_bundle[1],
-            x1=self.new_bundle[0],
+            x0=self.income_bundle[0],
+            y0=self.income_bundle[1],
+            x1=self.income_bundle[0],
             y1=0,
             line=dict(
                 color="grey",
@@ -244,6 +310,21 @@ class Slutsky:
                 width=1
             )
         )
+
+        if self.endowment_mode:
+            fig.add_shape(
+                type='line',
+                x0=self.endowment_income_bundle[0],
+                y0=self.endowment_income_bundle[1],
+                x1=self.endowment_income_bundle[0],
+                y1=0,
+                line=dict(
+                    color="grey",
+                    dash="dashdot",
+                    width=1
+                )
+            )
+
         fig.add_shape(
             type='line',
             xref='x',
@@ -270,13 +351,29 @@ class Slutsky:
                 dash='dashdot'
             )
         )
+
+        if self.endowment_mode:
+            fig.add_shape(
+                type='line',
+                xref='x',
+                yref='y',
+                x0=effect_boundaries[2],
+                y0=max_y_int / 15,
+                x1=effect_boundaries[3],
+                y1=max_y_int / 15,
+                line=dict(
+                    color='grey',
+                    dash='dashdot'
+                )
+            )
+
         fig.add_shape(
             type='line',
             xref='x',
             yref='y',
             x0=effect_boundaries[0],
             y0=max_y_int / 20,
-            x1=effect_boundaries[2],
+            x1=effect_boundaries[-1],
             y1=max_y_int / 20,
             line=dict(
                 color='grey',
@@ -297,9 +394,9 @@ class Slutsky:
         )
 
         fig.add_annotation(
-            x=(self.new_bundle[0] + self.substitution_bundle[0]) / 2,
+            x=(self.income_bundle[0] + self.substitution_bundle[0]) / 2,
             y=max_y_int / 15,
-            text='Income Effect',
+            text='Ordinary Income Effect',
             xref='x',
             yref='y',
             showarrow=True,
@@ -308,8 +405,21 @@ class Slutsky:
             ay=-20
         )
 
+        if self.endowment_mode:
+            fig.add_annotation(
+                x=(self.income_bundle[0] + self.endowment_income_bundle[0]) / 2,
+                y=max_y_int / 15,
+                text='Endowment Income Effect',
+                xref='x',
+                yref='y',
+                showarrow=True,
+                arrowhead=7,
+                ax=50,
+                ay=-20
+            )
+
         fig.add_annotation(
-            x=(effect_boundaries[2] + effect_boundaries[0]) / 2,
+            x=(effect_boundaries[-1] + effect_boundaries[0]) / 2,
             y=max_y_int / 20,
             text='Total Effect',
             xref='x',
